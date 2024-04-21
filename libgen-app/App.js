8766837a482b9a2e538f5bf0e4deba310c5d2717
Mai_Hai <maisonhai3@gmail.com>
2024-04-21 15:19:52 +0000

    Include ScrollView and test_data.

--- libgen-app/App.js
+++ libgen-app/App.js
@@ -1,8 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View} from 'react-native';
+import { ScrollView, StyleSheet, Text, View} from 'react-native';
 import { useEffect, useState } from 'react';
 
 import LibgenSearch from './search_react';
+import React from 'react';
+import testData from './test_data.json';
 
 const options = {
   mirror: 'http://gen.lib.rus.ec',
@@ -33,11 +35,25 @@ export default function App() {
 
   return (
     <>
-    <View style={{flex: 1, flexDirection: 'column', backgroundColor: "red"}}>
-      <View style={{flex: 0.3, flexDirection: 'column', backgroundColor: "green", alignItems:"center", justifyContent: "center"}}>
-        <Text style={{fontWeight: 'light'}} numberOfLines={1}>Lib<Text style={{fontWeight: 'bold'}}>Gen</Text> </Text> 
+      <View style={{flex: 1, flexDirection: 'column', backgroundColor: "red"}}>
+        <View style={{flex: 0.3, flexDirection: 'column', backgroundColor: "green", alignItems:"center", justifyContent: "center"}}>
+          <Text style={{fontWeight: 'light'}} numberOfLines={1}>Lib<Text style={{fontWeight: 'bold'}}>Gen</Text> </Text> 
+        </View>
+        <ScrollView style={{flex: 0.7, backgroundColor: 'gray', paddingHorizontal: 40, paddingVertical: 40}}>
+          <ScrollView style={{backgroundColor: 'gray', paddingHorizontal: 40, paddingVertical: 40}}>
+            {testData.map((item, index) => (
+              <View key={index} style={{backgroundColor: "blue", borderColor: "red", borderWidth: 1, padding: 10}}>
+                <Text>{item.language}</Text>
+                <Text>{item.lbc}</Text>
+                <Text>{item.lcc}</Text>
+                <Text>{item.library}</Text>
+                <Text>{item.local}</Text>
+                <Text>{item.locator}</Text>
+              </View>
+            ))}
+          </ScrollView>
+        </ScrollView>
       </View>
-    </View>
       {/* <View>
         {data.map((item, index) => (
           <View key={index}>
